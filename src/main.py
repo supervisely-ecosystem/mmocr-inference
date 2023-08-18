2b7d3b95bc85525ceff2f09d7b6a17753f475698
max-unfinity <poldersen2@gmail.com>
2023-08-18 16:30:28 +0000

    batch inference

--- src/main.py
+++ src/main.py
@@ -1,34 +1,33 @@
+import os
 from mmocr.apis import MMOCRInferencer
 import supervisely as sly
+import utils
 
 device = "cuda"
 batch_size = 2
+output_dir = "results"
+os.makedirs(output_dir, exist_ok=True)
 
 model = MMOCRInferencer(det="DBNetpp", rec='ABINet', device=device)
 
-
-predictions = model(["demo.png"], batch_size)["predictions"]
-
-img = sly.image.read("demo.png")
+image_paths = ["demo.png"]
+predictions = model(image_paths, batch_size)["predictions"]
 
 obj_class = sly.ObjClass("text_det", sly.Rectangle, [0, 255, 0])
-
-for pred in predictions:
+for pred, image_path in zip(predictions, image_paths):
+    img_name, _ = os.path.splitext(os.path.basename(image_path))
+    
+    # save texts
     texts = pred["rec_texts"]
-    print(texts)
+    sly.json.dump_json_file(texts, f"{output_dir}/{img_name}.json")
+
+    # draw visualizations (debug)
     labels = []
     for text, text_score, rect, rect_score in zip(*list(pred.values())):
-        # convert rectangles to sly.Rectangle
         assert len(rect) == 8
-        rect = list(map(int, rect))
-        x,y = rect[::2], rect[1::2]
-        xmin, ymin, xmax, ymax = min(x), min(y), max(x), max(y)
-        rect = sly.Rectangle(ymin, xmin, ymax, xmax)
-        label = sly.Label(rect, obj_class)
-        labels += [label]
+        label = utils.det_polygon_2_label(rect, obj_class)
+        labels.append(label)
+    img = sly.image.read(image_path)
     ann = sly.Annotation(img.shape[:2], labels)
-
-ann.draw_pretty(img, thickness=1)
-
-sly.image.write("demo_pred.jpg", img)
-sly.json.dump_json_file(texts, "texts.json")
+    ann.draw_pretty(img, thickness=1)
+    sly.image.write(f"{output_dir}/{img_name}_pred.jpg", img)
